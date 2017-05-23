535dfb413a6ae03b211c4f71e1c0b0a815e33bc5
Tim Price <tim.price@stfc.ac.uk>
2017-05-23 09:24:18 +0100

    Reset James striptool demo... (sorry for tweaking it)

--- Striptool/examples/striptool_Demo_Magnets_v2.py
+++ Striptool/examples/striptool_Demo_Magnets_v2.py
@@ -8,14 +8,10 @@ from pyqtgraph.dockarea import *
 import striptool as striptool
 import scatterPlot as scatterplot
 import numpy as np
-os.environ["EPICS_CA_AUTO_ADDR_LIST"] = "NO"
-os.environ["EPICS_CA_ADDR_LIST"] = "10.10.0.12" #BE SPECIFIC.... YOUR I.P. FOR YOUR VM
-os.environ["EPICS_CA_MAX_ARRAY_BYTES"] = "10000000"
-os.environ["EPICS_CA_SERVER_PORT"]="6000"
+
 
 ''' Load loggerWidget library (comment out if not available) '''
 sys.path.append(str(os.path.dirname(os.path.abspath(__file__)))+'\\..\\..\\loggerWidget\\')
-sys.path.append('\\\\fed.cclrc.ac.uk\\Org\\NLab\\ASTeC\\Projects\\VELA\\Software\\VELA_CLARA_PYDs\\bin\\stagetim')
 from ConfigParser import SafeConfigParser
 import loggerWidget as lw
 import logging
