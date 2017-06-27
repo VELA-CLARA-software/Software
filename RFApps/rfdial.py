9e8d59eea010d6a1d05cfb646496f04c7cb8c63c
James Jones <james.jones@stfc.ac.uk>
2017-06-27 17:41:19 +0100

    Pre-Merge

--- RFApps/rfdial.py
+++ RFApps/rfdial.py
@@ -1,62 +1,107 @@
-import sys
+import sys, math
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import epics
+
+class eSlider(QSlider):
+    def __init__(self, parent=None, min=-10, max=10, orientation=Qt.Horizontal, factor=1):
+        QSlider.__init__(self, parent)
+        self.factor = factor
+        self.min = min*self.factor
+        self.max = max*self.factor
+        self.range = self.max - self.min
+        self.setOrientation(orientation)
+        self.setValue(0)
+
+    def wheelEvent(self, evt):
+        modifiers = QApplication.keyboardModifiers()
+        val = self.value()
+        if modifiers == Qt.ControlModifier:
+            step = self.singleStep()/10
+        elif modifiers == Qt.ShiftModifier:
+            step = self.singleStep()*10
+        else:
+            step = self.singleStep()
+        step = math.copysign(step,evt.delta())
+        self.setValue(step+val)
+
+class eDoubleSpinBox(QDoubleSpinBox):
+    def __init__(self, parent=None, min=-10, max=10, factor=1):
+        QDoubleSpinBox.__init__(self, parent)
+        self.factor = factor
+        self.min = min*self.factor
+        self.max = max*self.factor
+        self.range = self.max - self.min
+        self.setValue(0)
+
+    def wheelEvent(self, evt):
+        modifiers = QApplication.keyboardModifiers()
+        val = self.value()
+        if modifiers == Qt.ControlModifier:
+            step = self.singleStep()/10
+        elif modifiers == Qt.ShiftModifier:
+            step = self.singleStep()*10
+        else:
+            step = self.singleStep()
+        step = math.copysign(step,evt.delta())
+        self.setValue(step+val)
 
 class Form(QDialog):
 
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
 
-        self.phasePV = epics.PV('CLA-GUNS-LRF-CTRL-01:vm:dsp:sp_ph:phase')
-        self.phasedial = QDial()
-        self.phasedial.setNotchesVisible(False)
-        self.phasedial.setRange(-180,180)
-        self.phasespinbox = QSpinBox()
-        self.phasespinbox.setRange(-180,180)
-        self.phasedial.setValue(self.phasePV.get())
-        self.phasespinbox.setValue(self.phasePV.get())
-        phaselayout = QHBoxLayout()
-        phaselayout.addWidget(self.phasedial)
-        phaselayout.addWidget(self.phasespinbox)
-        phasewidget = QWidget()
-        phasewidget.setLayout(phaselayout)
+        # self.phasePV = epics.PV('CLA-GUNS-LRF-CTRL-01:vm:dsp:sp_ph:phase')
+        # self.ampPV = epics.PV('CLA-GUNS-LRF-CTRL-01:vm:dsp:sp_amp:amplitude')
 
-        self.ampPV = epics.PV('CLA-GUNS-LRF-CTRL-01:vm:dsp:sp_amp:amplitude')
-        self.ampdial = QDial()
-        self.ampdial.setNotchesVisible(False)
-        self.ampdial.setRange(0,14500)
-        self.ampspinbox = QSpinBox()
-        self.ampspinbox.setRange(0,14500)
-        self.ampdial.setValue(self.ampPV.get())
-        self.ampspinbox.setValue(self.ampPV.get())
-        amplayout = QHBoxLayout()
-        amplayout.addWidget(self.ampdial)
-        amplayout.addWidget(self.ampspinbox)
-        ampwidget = QWidget()
-        ampwidget.setLayout(amplayout)
+        self.ampgroupbox = self.sliderAndSpinBox("RF Amplitude", 'CLA-GUNS-LRF-CTRL-01:vm:dsp:sp_amp:amplitude', 0, 14500, step=100)
+        self.phasegroupbox = self.sliderAndSpinBox("RF Phase", 'CLA-GUNS-LRF-CTRL-01:vm:dsp:sp_ph:phase' , -180, 180, step=1)
 
-        layout = QVBoxLayout()
-        layout.addWidget(ampwidget)
-        layout.addWidget(phasewidget)
+        layout = QHBoxLayout()
+        layout.addWidget(self.ampgroupbox)
+        layout.addWidget(self.phasegroupbox)
 
         self.setLayout(layout)
 
-        self.phasedial.valueChanged.connect(self.phasespinbox.setValue)
-        self.phasespinbox.valueChanged.connect(self.phasedial.setValue)
-        self.phasespinbox.valueChanged.connect(self.setRFPhase)
+        self.setWindowTitle("RF Controls")
 
-        self.ampdial.valueChanged.connect(self.ampspinbox.setValue)
-        self.ampspinbox.valueChanged.connect(self.ampdial.setValue)
-        self.ampspinbox.valueChanged.connect(self.setRFAmplitude)
+    def magnetController(self, name):
+        pass
 
-        self.setWindowTitle("RF Dials")
+    def sliderAndSpinBox(self, name, pvname, min, max, step=1, sliderfactor=10):
+        pv = epics.PV(pvname)
+        slider = eSlider(min=min, max=max, factor=sliderfactor, orientation=Qt.Horizontal)
+        slider.setRange(min*sliderfactor, max*sliderfactor)
+        slider.setValue(pv.get()*sliderfactor)
+        slider.setSingleStep(sliderfactor*step)
+        slider.setPageStep(sliderfactor*step*10)
 
-    def setRFAmplitude(self):
-        self.ampPV.put(self.ampspinbox.value())
+        spinbox = eDoubleSpinBox()
+        spinbox.setRange(min,max)
+        spinbox.setValue(pv.get())
+        spinbox.setSingleStep(step)
+        # spinbox.setPageStep(step*10)
 
-    def setRFPhase(self):
-        self.phasePV.put(self.phasespinbox.value())
+        layout = QHBoxLayout()
+        layout.addWidget(slider)
+        layout.addWidget(spinbox)
+        widget = QWidget()
+        widget.setLayout(layout)
+        groupbox = QGroupBox(self)
+        groupbox.setTitle(name)
+        groupbox.setLayout(layout)
+        slider.valueChanged.connect(lambda x: spinbox.setValue(float(x)/float(sliderfactor)))
+        spinbox.valueChanged.connect(lambda x: slider.setValue(float(x)*float(sliderfactor)))
+        spinbox.valueChanged.connect(lambda x: self.setPV(pv, x))
+        epics.camonitor(pvname, callback=lambda **x: self.updateControls(slider,spinbox,**x))
+        return groupbox
+
+    def updateControls(self, slider, spinbox, **kwargs):
+        spinbox.setValue(kwargs['value'])
+
+    def setPV(self, pv, val):
+        # pass
+        pv.put(val)
 
 app = QApplication(sys.argv)
 form = Form()
