e13fea9dbcd55452d57a148f0938006efc6c7054
Ben Shepherd <bjashepherd@gmail.com>
2017-09-29 11:12:19 +0100

    [cassis] improvements after control room testing

--- Apps/cassis/cassis.py
+++ Apps/cassis/cassis.py
@@ -5,6 +5,10 @@
 This app scans the gun gradient and solenoid and bucking coil currents, at each point taking an image of the screen
 and grabbing traces from the wall current monitor and beam loss monitors via the Q-scope."""
 
+#TODO: what are time units for plots?
+#TODO: make link to work folder clickable
+#TODO: make sure Github has up-to-date version of cdsbox1 script
+
 import sys
 from PyQt4 import QtCore, QtGui, uic
 import os
@@ -15,7 +19,6 @@ import pyqtgraph as pg
 from cassis_model import DarkCurrentScan
 from time import gmtime, strftime
 import numpy as np
-from libtiff import TIFF
 
 os.environ["EPICS_CA_AUTO_ADDR_LIST"] = "NO"
 os.environ["EPICS_CA_MAX_ARRAY_BYTES"] = "10000000"
@@ -37,6 +40,8 @@ Ui_MainWindow, QtBaseClass = uic.loadUiType("cassis.ui")
 
 
 class CassisApp(QtGui.QMainWindow, Ui_MainWindow):
+    plotsUpdated = QtCore.pyqtSignal(list, list, list, list, np.ndarray)
+
     def __init__(self):
         #TODO: get initial parameters from INI file, and save them as we go
         QtGui.QMainWindow.__init__(self)
@@ -44,7 +49,7 @@ class CassisApp(QtGui.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         [label.setText('') for label in (self.mom_now_label, self.sol_now_label, self.bc_now_label)]
-        self.progressBar.setValue(0)
+        self.progress_bar.setValue(0)
 
         self.wcm_plot.setLabels(title='Wall current monitor', left='voltage [V]', bottom='time [µs]')
         self.wcm_plot.showGrid(True, True)
@@ -53,11 +58,12 @@ class CassisApp(QtGui.QMainWindow, Ui_MainWindow):
 
         self.go_button.clicked.connect(self.goButtonClicked)
         self.cancel_button.clicked.connect(self.cancelButtonClicked)
+        self.plotsUpdated.connect(self.updatePlots)
 
         self.magInit = MagCtrl.init()
         self.scopeInit = Scope_Control.init()
         self.machine_mode_dropdown.activated.connect(self.machineModeChanged)
-        self.machineModeChanged()
+        self.mode_set = False
         self.update_period = 100  # milliseconds
         self.startMainViewUpdateTimer()
 
@@ -65,12 +71,20 @@ class CassisApp(QtGui.QMainWindow, Ui_MainWindow):
         # filename = r'\\fed.cclrc.ac.uk\Org\NLab\ASTeC\Projects\VELA\Work\2017\09\27\0911-dark-current\sol-100-new.tif'
         # self.screen_image.setImage(TIFF.open(filename).read_image())
 
+    def showEvent(self, event):
+        app.processEvents()
+
     # these functions update the GUI and (re)start the timer
     def startMainViewUpdateTimer(self):
         self.widgetUpdateTimer = QtCore.QTimer()
         self.widgetUpdateTimer.timeout.connect(self.mainViewUpdate)
         self.widgetUpdateTimer.start(self.update_period)
     def mainViewUpdate(self):
+        if not self.mode_set:
+            # This can be a long operation if we're connecting to EPICS - don't do it until the window is displayed.
+            self.machineModeChanged()
+            self.mode_set = True
+
         # conceivably the timer could restart this function before it complete - so guard against that
         try:
             if not self.machine_mode == 'Offline':
@@ -80,25 +94,38 @@ class CassisApp(QtGui.QMainWindow, Ui_MainWindow):
 
     def goButtonClicked(self):
         """Start recording dark current data."""
+        self.statusBar().showMessage('Starting scan...')
         self.work_folder = r'\\fed.cclrc.ac.uk\Org\NLab\ASTeC\Projects\VELA\Work' + \
                       strftime("\\%Y\\%m\\%d\\%H%M-dark-current", gmtime())
         os.makedirs(self.work_folder)
         self.work_folder_label.setText('Data saved to <a href="{}">work folder</a>'.format(self.work_folder))
         with open(self.work_folder + '\\metadata.txt', 'w') as f:
-            f.write('Machine: ' + self.machine_dropdown.currentText())
-            f.write('Gun: ' + self.gun_dropdown.currentText())
-            f.write('Cathode: ' + self.cathode_dropdown.currentText())
-            f.write('RF pulse width [µs]: ' + self.pulse_width_spinbox.value())
-            f.write(self.vacuum_label.text())
+            f.writelines('Machine: ' + self.machine_dropdown.currentText())
+            f.writelines('Gun: ' + self.gun_dropdown.currentText())
+            f.writelines('Cathode: ' + self.cathode_dropdown.currentText())
+            f.writelines('RF pulse width [µs]: {:.1f}'.format(self.pulse_width_spinbox.value()))
+            f.writelines(self.vacuum_label.text())
         self.scan = DarkCurrentScan(self.magnet_controller, self.scope_controller, self.work_folder)
         mom_range = np.arange(self.mom_from_spinbox.value(), self.mom_to_spinbox.value() + 1e-3, self.mom_step_spinbox.value())
         sol_range = np.arange(self.sol_from_spinbox.value(), self.sol_to_spinbox.value() + 1e-3, self.sol_step_spinbox.value())
         bc_range = np.arange(self.bc_from_spinbox.value(), self.bc_to_spinbox.value() + 1e-3, self.bc_step_spinbox.value())
+        self.firstPoint = True
+        self.toggleControls(True)
         self.scan.startLoop(mom_range, sol_range, bc_range, self.updateProgress)
+
+    def toggleControls(self, scan_running):
+        [control.setEnabled(not scan_running) for control in
+         (self.mom_from_spinbox, self.mom_to_spinbox, self.mom_step_spinbox,
+          self.sol_from_spinbox, self.sol_to_spinbox, self.sol_step_spinbox,
+          self.bc_from_spinbox, self.bc_to_spinbox, self.bc_step_spinbox,
+          self.go_button)]
+        self.cancel_button.setEnabled(scan_running)
 
     def cancelButtonClicked(self):
         """Cancel the dark current recording process."""
         self.scan.stopLoop()
+        self.toggleControls(False)
+        self.statusBar().showMessage('Scan cancelled.')
 
     def updateProgress(self, scan_params, traces, sc1_img, progress):
         """Callback function from the DC recording thread. Update the parameters, progress and plots."""
@@ -108,23 +135,36 @@ class CassisApp(QtGui.QMainWindow, Ui_MainWindow):
         self.mom_now_label.setText('now: {:.1f} MeV'.format(mom))
         self.sol_now_label.setText('now: {:.1f} A'.format(sol_current))
         self.bc_now_label.setText('now: {:.1f} A'.format(bc_current))
-        self.progress_bar.setValue(progress)
-        # update plots and screen image on window
-        self.wcm_plot.plot(wcm1, clear=True)
-        self.wcm_plot.plot(wcm2)
-        self.blm_plot.plot(blm1, clear=True)
-        self.blm_plot.plot(blm2)
-        self.screen_image.setImage(sc1_img)
+        self.progress_bar.setValue(progress * 100)
+        progress_text = 'Scanning ({:.0f}% complete): momentum {:.1f} MeV, solenoid {:.1f} A, bucking coil {:.1f} A'
+        self.statusBar().showMessage(progress_text.format(progress * 100, mom, sol_current, bc_current))
+        self.plotsUpdated.emit(wcm1, wcm2, blm1, blm2, sc1_img)
         # save all data to work folder (image is already saved)
-        file_prefix = '\\mom={:.1f}_sol={:.1f}_bc={.1f}'.format(mom, sol_current, bc_current)
+        file_prefix = '\\mom={:.1f}_sol={:.1f}_bc={:.1f}'.format(mom, sol_current, bc_current)
         np.savetxt(self.work_folder + file_prefix + '_wcm1.csv', wcm1, delimiter=',')
         np.savetxt(self.work_folder + file_prefix + '_wcm2.csv', wcm2, delimiter=',')
         np.savetxt(self.work_folder + file_prefix + '_blm1.csv', blm1, delimiter=',')
         np.savetxt(self.work_folder + file_prefix + '_blm2.csv', blm2, delimiter=',')
+        self.firstPoint = False
+        if progress == 1:
+            self.toggleControls(False)
+
+    def updatePlots(self, wcm1, wcm2, blm1, blm2, sc1_img):
+        # update plots and screen image on window (in the GUI thread)
+        self.wcm_plot.plot(wcm1, pen='y', clear=True)
+        self.wcm_plot.plot(wcm2, pen=(255, 0, 128))  # pink
+        self.blm_plot.plot(blm1, pen='c', clear=True)
+        self.blm_plot.plot(blm2, pen='g')
+        self.screen_image.setImage(sc1_img, autoLevels=self.firstPoint)  # only reset the levels on the first point
 
     def machineModeChanged(self, index=None):
         mode = str(self.machine_mode_dropdown.currentText())
+        QtGui.QApplication.setOverrideCursor(QtCore.Qt.WaitCursor)
+        self.statusBar().showMessage('Setting {} machine mode...'.format(mode.lower()))
+        app.processEvents()
         self.setMachineMode(mode)
+        self.statusBar().showMessage('Ready.')
+        QtGui.QApplication.restoreOverrideCursor()
 
     def setMachineMode(self, mode=None):
         self.machine_mode = mode
@@ -134,7 +174,7 @@ class CassisApp(QtGui.QMainWindow, Ui_MainWindow):
         # https://astec-team.slack.com/archives/C2Z5G1X97/p1506330922000140
         self.magnet_controller = self.magInit.getMagnetController(MagCtrl.MACHINE_MODE.names[mode.upper()], MagCtrl.MACHINE_AREA.VELA_INJ)
         self.scope_controller = self.scopeInit.getScopeController(MagCtrl.MACHINE_MODE.names[mode.upper()], MagCtrl.MACHINE_AREA.VELA_INJ)
-        scopeController.setBufferSize(1)
+        self.scope_controller.setBufferSize(50)
         # self.settings.setValue('machine_mode', mode)
 
 if __name__ == "__main__":
--- Apps/cassis/cassis_model.py
+++ Apps/cassis/cassis_model.py
@@ -12,13 +12,13 @@ from libtiff import TIFF
 from time import time, sleep
 
 context = zmq.Context()
-socket = context.socket(zmq.PAIR)
-socket.connect('tcp://cdsvbox1:5556')
 
 
 class DarkCurrentScan(threading.Thread):
     def __init__(self, mag_controller, scope_controller, save_location):
         super(DarkCurrentScan, self).__init__()
+        self.socket = context.socket(zmq.PAIR)
+        self.socket.connect('tcp://cdsvbox1:5556')
         self.magnets = mag_controller
         self.scopes = scope_controller
         self.save_location = save_location
@@ -31,43 +31,47 @@ class DarkCurrentScan(threading.Thread):
         self.sol_range = sol_range
         self.bc_range = bc_range
         self.n_points = len(mom_range) * len(sol_range) * len(bc_range)
-        self.run(callback)
+        self.callback = callback
+        self.start()
 
     def stopLoop(self):
         self.cancel = True
 
-    def run(self, callback):
+    def run(self):
         #TODO: get gun gradient
         sol_current = self.magnets.getSI('SOL')
         bc_current = self.magnets.getSI('BSOL')
-        self.run_loop(callback)
-        self.magnets.setSI('SOL', sol_current)
-        self.magnets.setSI('BSOL', bc_current)
+        self.run_loop()
+        # self.magnets.setSI('SOL', sol_current)
+        # self.magnets.setSI('BSOL', bc_current)
 
-    def run_loop(self, callback):
+    def run_loop(self):
         i = 0
         for mom in self.mom_range:
+            print('Setting momentum: ', mom)
             #TODO: lookup momentum and set gun gradient accordingly
-            for sol_current in self.parent.sol_range:
-                self.magnets.setSI('SOL', sol_current)
-                for bc_current in self.parent.bc_range:
-                    self.magnets.setSI('BSOL', bc_current)
+            for sol_current in self.sol_range:
+                print('Setting SOL current:', sol_current)
+                # self.magnets.setSI('SOL', sol_current)
+                for bc_current in self.bc_range:
+                    print('Setting BSOL current: ', bc_current)
+                    # self.magnets.setSI('BSOL', bc_current)
 
-                    filename = '{}\\mom={:.1f}_sol={:.1f}_bc={.1f}.tif'.format(
+                    filename = '{}\\mom={:.1f}_sol={:.1f}_bc={:.1f}.tif'.format(
                         self.save_location, mom, sol_current, bc_current)
                     traces, img = self.singleGrab(filename)
                     i += 1
-                    callback((mom, sol_current, bc_current), traces, img, i / self.n_points)
+                    self.callback((mom, sol_current, bc_current), traces, img, i / float(self.n_points))
                     if self.cancel:
                         return
 
     def singleGrab(self, filename):
         """Do a single dark current point."""
-        wcm1 = self.scopes.getScopeTR1Buffer("WVF01")[0]
-        wcm2 = self.scopes.getScopeTR2Buffer("WVF01")[0]
-        blm1 = self.scopes.getScopeTR3Buffer("WVF01")[0]
-        blm2 = self.scopes.getScopeTR4Buffer("WVF01")[0]
-        # socket.send(filename)  # send a signal to cdsvbox1 to take an image, and save it
+        wcm1 = list(self.scopes.getScopeTR1Buffer("WVF01")[0])
+        wcm2 = list(self.scopes.getScopeTR2Buffer("WVF01")[0])
+        blm1 = list(self.scopes.getScopeTR3Buffer("WVF01")[0])
+        blm2 = list(self.scopes.getScopeTR4Buffer("WVF01")[0])
+        self.socket.send(filename)  # send a signal to cdsvbox1 to take an image, and save it
         # wait until file exists and is reasonable size
         start = time()
         while time() - start < 10:  # wait 10 secs
@@ -78,8 +82,9 @@ class DarkCurrentScan(threading.Thread):
             except WindowsError:  # ignore "file not found"
                 pass
         else:  # got to the end of the loop!
-            raise FileNotFoundError('Timed out waiting for the file "{}" to be created.'.format(filename))
+            raise  # re-raise the file not found error
         img = TIFF.open(filename).read_image()
         # resave with compression (approx 3x smaller files!)
+        #TODO: farm this off to a new thread
         TIFF.open(filename, mode='w').write_image(img, compression='lzw')
         return (wcm1, wcm2, blm1, blm2), img
