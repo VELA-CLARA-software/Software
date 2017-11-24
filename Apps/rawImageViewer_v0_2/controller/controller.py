7c75dbf5a6fbdf6b5937ddfacc67cc50c610aaf9
Tim Price <tim.price@stfc.ac.uk>
2017-11-24 09:29:37 +0000

    edit to viewer and periodically image taking script

--- Apps/rawImageViewer_v0_2/controller/controller.py
+++ Apps/rawImageViewer_v0_2/controller/controller.py
@@ -2,10 +2,6 @@ from PyQt4 import QtCore
 from PyQt4 import QtGui
 from PyQt4 import Qt
 
-import os
-import time
-import urllib
-import webbrowser
 import numpy as np
 import pyqtgraph as pg
 
@@ -32,35 +28,35 @@ class Controller():
         self.view.pushButton.clicked.connect(self.openImageDir)
 
 ## Create a GL View widget to display data
-        #self.w = gl.GLViewWidget()
-        #self.w.show()
+        self.w = gl.GLViewWidget()
         #self.w.setWindowTitle('pyqtgraph example: GLSurfacePlot')
-        #self.w.setCameraPosition(distance=50)
-
+        self.w.setCameraPosition(distance=250)
+        self.imarray = np.zeros((2560,2160))
+        x = np.linspace(0,256,256)
+        y = np.linspace(0,216,216)
+        z = np.divide(self.imarray[0:256,0:216],65535/10) #np.random.normal(size=(216,256))#pg.gaussianFilter(np.random.normal(size=(216,256)), (1,1))
+        self.p3d = gl.GLSurfacePlotItem(x=x, y=y, z=z, shader='heightColor', computeNormals=False, smooth=False)
         ## Add a grid to the view
         #g = gl.GLGridItem()
         #g.scale(2,2,1)
         #g.setDepthValue(100)  # draw grid after surfaces since they may be translucent
+        self.w.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        g = gl.GLGridItem()
         #self.w.addItem(g)
         ## Simple surface plot example
         ## x, y values are not specified, so assumed to be 0:50
 
-        self.roi = pg.ROI([0,0], [216,256])
-        self.roi.addScaleHandle([0.5, 0], [0.5, 0.5])
-        self.roi.addScaleHandle([1, 0.5], [0.5, 0.5])
+        self.roi = pg.ROI([0,0], [256,216])
+        #self.roi.addScaleHandle([0.5, 0], [0.5, 0.5])
+        #self.roi.addScaleHandle([1, 0.5], [0.5, 0.5])
 
         self.ImageBox = layout.addViewBox(lockAspect=True, colspan=2)
         self.Image = pg.ImageItem(np.random.normal(size=(2560, 2160)))
-        #self.ImageBox2 = layout.addViewBox(lockAspect=True, colspan=2)
         self.ImageBox.addItem(self.Image)
-        #self.OOPLot = gl.GLGraphicsItem.GLGraphicsItem()
-        #self.OOPLot.setDepthValue(10)
-        #self.ImageBox2.addItem(self.OOPLot)
+
         self.view.gridLayout.addWidget(monitor, 0, 2, 6, 1)
-        #w = gl.GLViewWidget()
-        #w.setCameraPosition(distance=50)
-        #w.show()
-        #self.view.gridLayout.addWidget(w, 0, 3, 6, 1)
+        self.view.gridLayout.addWidget(self.w, 0, 3, 6, 1)
+
         STEPS = np.linspace(0, 1, 4)
         CLRS = ['k', 'r', 'y', 'w']
         a = np.array([pg.colorTuple(pg.Color(c)) for c in CLRS])
@@ -79,26 +75,28 @@ class Controller():
         f = open(filename, "r")
         a = np.fromfile(f, dtype=np.uint16)
         a = a.reshape((2160, 2560))
-        self.imarray = np.array(a)
-        self.Image.setImage(np.flip(np.transpose(self.imarray), 1))
-        #x = np.linspace(0,256,256)
-        ##y = np.linspace(0,256,256)
-        #z = np.divide(self.imarray[0:216,0:256],65535/10) #np.random.normal(size=(216,256))#pg.gaussianFilter(np.random.normal(size=(216,256)), (1,1))
-        #self.p1 = gl.GLSurfacePlotItem(x=x, y=y, z=z, shader='heightColor', computeNormals=False, smooth=False)
-        #self.p1.setData(x=x,y=y,z=z)
-        #p1.shader()['colorMap'] = np.array([0.2, 2, 0.5, 0.2, 1, 1, 0.2, 0, 2])
-        #p1.scale(1.0, 1.0, 1.0/65535.0)
-        #self.p1.translate(-108, -128, 0)
-        #self.w.addItem(self.p1 )
-        #self.w.show()
-        #self.ImageBox.addItem(self.roi)
+        self.imarray = np.flip(np.transpose(np.array(a)), 1)
+        self.Image.setImage(self.imarray)
+        x = np.linspace(0,256,256)
+        y = np.linspace(0,216,216)
+        z = np.divide(self.imarray[0:256,0:216], self.view.spinBox_max.value())
+        self.p3d = gl.GLSurfacePlotItem(x=x, y=y, z=z, shader='shaded', computeNormals=True, smooth=False) #color=(0.5, 0.5, 1, 50)
+        #self.p3d.shader()['colorMap'] = np.array([0.2, 2, 0.5, 0.2, 1, 1, 0.2, 0, 2])
+        #self.p3d.shader()['colorMap'] = np.array([])
+        self.p3d.setData(x=x,y=y,z=z)
+        self.p3d.translate(-128, -108, 0)
+        self.w.addItem(self.p3d )
+        self.ImageBox.addItem(self.roi)
         self.view.pushButton.setText('Load Image')
         #self.roi.setZValue(12)
     def update(self):
         self.Image.setLevels([self.view.spinBox_min.value(), self.view.spinBox_max.value()], update=True)
-        #x = np.linspace(0,int(self.roi.size()[0]),int(self.roi.size()[0]))
-        #y = np.linspace(0,int(self.roi.size()[1]),int(self.roi.size()[1]))
-        #z = np.divide(self.imarray[int(self.roi.pos()[0]):int(self.roi.pos()[0])+int(self.roi.size()[0]),
-        #int(self.roi.pos()[1]):int(self.roi.pos()[1])+int(self.roi.size()[1])],65535/10)
-        #self.p1.setData(x=x,y=y,z=z)
-        #self.w.show()
+
+        x = np.linspace(0,int(self.roi.size()[0]),int(self.roi.size()[0]))
+        y = np.linspace(0,int(self.roi.size()[1]),int(self.roi.size()[1]))
+        newArray = self.imarray[int(self.roi.pos()[0]):int(self.roi.pos()[0])+int(self.roi.size()[0]),
+        int(self.roi.pos()[1]):int(self.roi.pos()[1])+int(self.roi.size()[1])]
+        z = np.divide(newArray, np.amax(newArray)/50)
+        #self.p3d.shader()['colorMap'] = np.linspace(0,np.amax(newArray)/50,5)
+        #self.p3d.scale(1., 1., 1.0)
+        self.p3d.setData(x=x,y=y,z=z)
--- Procedures/shiftImageSave/script.py
+++ Procedures/shiftImageSave/script.py
@@ -9,16 +9,22 @@ cameras = Init.physical_CLARA_Camera_DAQ_Controller()
 
 onShift = True
 
+#os.system('mkdir \\\\fed.cclrc.ac.uk\\Org\\NLab\\ASTeC\\Projects\\VELA\\Work\\'+time.strftime("%Y")+'\\'+time.strftime("%m")+'\\'+time.strftime("%d")+'\\VirtualCathode_Images\\')
+
 while onShift:
-    cameras.startVCAcquiring()
+    leaveAcquiring=True
+    if cameras.isAcquiring('VC')==False:
+        leaveAcquiring=False
+        cameras.startVCAcquiring()
     time.sleep(1)
     cameras.collectAndSaveVC(1)
 
     # Stop acquiring
     time.sleep(1)
-    cameras.stopVCAcquiring()
-    os.system('copy \\\\fed.cclrc.ac.uk\\Org\\NLab\\ASTeC\\Projects\\VELA\\Work\\2017\\VirtualCathodeCamera\\VCA-CAM-01_001.bin \\\\fed.cclrc.ac.uk\\Org\\NLab\\ASTeC\\Projects\\VELA\Work\\2017\\todaysVirtualCathodeImages\\VCimage_'+ time.strftime("%Y-%m-%d_%H-%M-%S")+'.bin')
-
+    if leaveAcquiring==False:
+        cameras.stopVCAcquiring()
+    os.system('copy \\\\fed.cclrc.ac.uk\\Org\\NLab\\ASTeC\\Projects\\VELA\\Work\\2017\\VirtualCathodeCamera\\VCA-CAM-01_001.bin \\\\fed.cclrc.ac.uk\\Org\\NLab\\ASTeC\\Projects\\VELA\\Work\\2017\\todaysVirtualCathodeImages\\VCimage_'+ time.strftime("%Y-%m-%d_%H-%M-%S")+'.bin')
+    #time.sleep(10)
     time.sleep(600)
     if int(time.strftime("%H"))>=17:
         if  int(time.strftime("%M"))>0:
