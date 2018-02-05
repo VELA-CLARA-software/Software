63db51fb22dbda9c5d5beb14fbc5b598a81b21e8
Ben Shepherd <bjashepherd@gmail.com>
2018-02-05 10:41:51 +0000

    [machineStatus] code cleanup

--- elog/machine-status/machineStatusServer.py
+++ elog/machine-status/machineStatusServer.py
@@ -10,18 +10,21 @@ version_history = {
 version = max(version_history.keys())
 
 import http.server
-import ssl
+import os
 import re
-import sys, os
-# os.environ["EPICS_CA_ADDR_LIST"] = "192.168.83.255"  # CLARA EPICS gateway
+import ssl
+import sys
+
 if os.name == 'posix':
     # Help pyepics find the right library
     os.environ['PYEPICS_LIBCA'] = '/home/opt/EPICS/base/lib/linux-x86_64/libca.so'
+else:  # network broadcast works on anywhere but apsv2
+    os.environ["EPICS_CA_ADDR_LIST"] = "192.168.83.255"  # CLARA EPICS gateway
+
 from epics import PV
 from datetime import datetime
-from time import sleep
 from collections import OrderedDict
-#from SysTrayIcon import SysTrayIcon
+
 try:
     from urllib import parse
 except ImportError:  # for Python 2
@@ -40,22 +43,21 @@ class StoppableServer(http.server.HTTPServer):
 
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
-
     params = {}
     pvs = {}
 
     # Read in HTML snippets for status tables 
-    # Filenames should be "status.nn.Region.html"
+    # File names should be "status.nn.Region.html"
     # where nn is a number that will be used for sorting
     # Region names will be made into URLs, so no funny characters (like spaces)
-    status_files = sorted([file for file in os.listdir('.') 
+    status_files = sorted([file for file in os.listdir('.')
                            if file.startswith('status.') and file.endswith('.html')])
     # A normal dict would do fine, except we want a particular order for the menu
     output = OrderedDict()
     for filename in status_files:
         region = filename.split('.')[2]
         output[region] = open(filename).read()
-        
+
     # Define HTML headers and footers
     # Note the {{}} here - this will be replaced by the machine region
     # We also add a menu of all the regions, hence the final <p>
@@ -64,7 +66,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 
     # Parse output tables
     # Most PVs contain colon characters (:), but the format string won't parse these correctly.
-    # We replace them here with a backtick (`), assuming this will never be found in a PV name.
+    # We replace them here with a back tick (`), assuming this will never be found in a PV name.
     # We also replace a dot (.) with a NOT sign (¬)
     # The format code thus always needs to have a final colon, and a specifier
     # Valid examples: {INJ-MAG-DIP-01:SI:.3f}, {INJ-LSR-DLY-01:BCAL:s}
@@ -72,7 +74,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
     # (Some special cases exist where formatting is not enough to produce a
     #  nice output - see below.)
 
-    curlyBrackets = re.compile(r'{([^}]*):(.*?)}') #match PV name and format spec
+    curlyBrackets = re.compile(r'{([^}]*):(.*?)}')  # match PV name and format spec
     # use a new dict to store lower case region names
     # and correctly parsed PV names
     newOutput = {}
@@ -84,7 +86,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             # add connection to this PV (if not already done)
             escapedParam = param.replace(':', '`')
             escapedParam = escapedParam.replace('.', '¬')
-            if not escapedParam in pvs:
+            if escapedParam not in pvs:
                 pvs[escapedParam] = PV(param)
             text = text.replace('{' + param + ':' + formatCode + '}',
                                 '{' + escapedParam + ':' + formatCode + '}', 1)
@@ -92,7 +94,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         newOutput[region.lower()] = text
     output = newOutput
 
-    header = header[0:-2] + '</p>' #chop last |
+    header = header[0:-2] + '</p>'  # chop last |
 
     returnTypeRE = re.compile(r'\.(json|html)$')
 
@@ -100,67 +102,67 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        
+
     def do_GET(self):
-        path = self.path[1:].lower() #strip leading /, case insensitive
-        pathSplit = self.returnTypeRE.split(path)
-        returnType = pathSplit[1] if len(pathSplit) > 1 else ''
-        path = pathSplit[0]
-        htmlOutput = (returnType == 'html' or path == '')
+        path = self.path[1:].lower()  # strip leading /, case insensitive
+        path_split = self.returnTypeRE.split(path)
+        return_type = path_split[1] if len(path_split) > 1 else ''
+        path = path_split[0]
+        html_output = (return_type == 'html' or path == '')
 
         if path in self.output:
-            nowString = datetime.now().strftime('%d/%m/%y %H:%M:%S')
-            pvValues = {'timestamp': nowString}
+            now_string = datetime.now().strftime('%d/%m/%y %H:%M:%S')
+            pv_values = {'timestamp': now_string}
             for param in self.params[path]:
-#                print(param)
+                #                print(param)
                 pv = self.pvs[param]
                 print(pv)
                 if not pv.connected:
-                    pvVal = float('nan')
+                    pv_val = float('nan')
                 else:
-                    pvVal = pv.get()
+                    pv_val = pv.get()
                 # handle special cases
                 # report a nicely formatted value for the train length
                 # in micro- or nanoseconds
                 # if param == 'INJ-LSR-DLY-01`BCAL':
-                    # if pvVal < 1:
-                        # pvVal = '{:.0f} ns'.format(pvVal * 1000)
-                    # elif pvVal >= 1:
-                        # pvVal = '{:.0f} &mu;s'.format(pvVal)
-                    # else:
-                        # pvVal = 'N/A'
+                # if pv_val < 1:
+                # pv_val = '{:.0f} ns'.format(pv_val * 1000)
+                # elif pv_val >= 1:
+                # pv_val = '{:.0f} &mu;s'.format(pv_val)
+                # else:
+                # pv_val = 'N/A'
 
                 # status of burst generator
                 # elif param == 'INJ-LSR-POC-01`RRUN':
-                    # if pvVal == -1:
-                        # pvVal = 'On'
-                    # elif pvVal == 0:
-                        # pvVal = 'Off'
-                    # else:
-                        # pvVal = 'N/A'
+                # if pv_val == -1:
+                # pv_val = 'On'
+                # elif pv_val == 0:
+                # pv_val = 'Off'
+                # else:
+                # pv_val = 'N/A'
 
-                pvValues[param] = pvVal
+                pv_values[param] = pv_val
 
-#            print(self.output[path])
-#            print(pvValues)
-            output = self.output[path].format(nowString, **pvValues)
-            if htmlOutput:
-                contentType = "text/html"
+            #            print(self.output[path])
+            #            print(pv_values)
+            output = self.output[path].format(now_string, **pv_values)
+            if html_output:
+                content_type = "text/html"
                 output = self.header.format(path + ' - ') + output + self.footer
             else:
-#                contentType = 'text/javascript'
-                contentType = "text/html"
+                #                content_type = 'text/javascript'
+                content_type = "text/html"
                 output = '''elogInsertMachineStatus(
                             {{"status": "ok",
                               "area": "{0}",
                               "params": '{1}'}})'''.format(path, output.replace('\n', ''))
 
         elif path == '':
-            contentType = "text/html"
+            content_type = "text/html"
             output = self.header.format('') + self.footer
 
         elif path[0:5] == 'work/':
-            contentType = 'text/javascript'
+            content_type = 'text/javascript'
             # List PNG files in specified work folder, in JSON format
             # This is used to provide the eLog with a built-in image browser
             folder = parse.unquote('//dlfiles03/alice/' + path)
@@ -177,38 +179,39 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         elif path == 'restart':
             # restart the server
             # serve a redirect page first
-            contentType = "text/html"
+            content_type = "text/html"
             output = open('restart.html', 'r').read().format(app_name=app_name)
             self.server.running = False
 
-#        elif path == 'favicon.ico':
-#            # Serve a favicon (this doesn't work)
-#            contentType = "image/ico"
-#            output = open('Alice_Logo.ico', 'rb').read()
+        #        elif path == 'favicon.ico':
+        #            # Serve a favicon (this doesn't work)
+        #            content_type = "image/ico"
+        #            output = open('Alice_Logo.ico', 'rb').read()
 
         else:
             # Default response
-            contentType = "text/html"
+            content_type = "text/html"
             output = (self.header.format('') +
                       '<p>Invalid URL: /{0}</p>'.format(path) + self.footer)
 
         self.send_response(200)
-        self.send_header("Content-type", contentType)
+        self.send_header("Content-type", content_type)
         self.end_headers()
         self.wfile.write(output.encode('utf-8'))
 
-try:
-    port = 27643
-    server = StoppableServer(('', port), MyHandler)
 
-    certfile = '/etc/pki/tls/certs/apsv2-dl-ac-uk.crt'
-    keyfile = '/home/bjs54/apsv2-dl-ac-uk.key'
-    try:
-        server.socket = ssl.wrap_socket(server.socket, keyfile=keyfile, certfile=certfile, server_side=True)
-        protocol = 'https'
-    except IOError:
-        protocol = 'http'
-    print('Started server: try {protocol}://localhost:{port}/'.format(**locals()))
+port = 27643
+server = StoppableServer(('', port), MyHandler)
+
+certfile = '/etc/pki/tls/certs/apsv2-dl-ac-uk.crt'
+keyfile = '/home/bjs54/apsv2-dl-ac-uk.key'
+try:
+    server.socket = ssl.wrap_socket(server.socket, keyfile=keyfile, certfile=certfile, server_side=True)
+    protocol = 'https'
+except IOError:
+    protocol = 'http'
+print('Started server: try {protocol}://localhost:{port}/'.format(**locals()))
+try:
     server.serve_forever()  # until /restartServer requested
     del(server)
 
@@ -223,4 +226,3 @@ try:
 except KeyboardInterrupt:
     print('^C received, shutting down server')
     server.socket.close()
-
