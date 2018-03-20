a61fafaf98d7c2d91177917758e8625e1cb0f058
Alex Brynes <alexander.brynes@stfc.ac.uk>
2018-03-20 10:32:54 +0000

    [BPMCalibration] added delay calibration
    Tied it all into one app

---
+++ Apps/BPMCalibration/controllers/bpm_handler.py
@@ -0,0 +1,89 @@
+from bpm_handler_base import bpm_handler_base
+import data.bpm_calibrate_data_base as dat
+import numpy
+
+class bpm_handler(bpm_handler_base):
+    #whoami
+    my_name= 'bpm_handler'
+    def __init__(self):
+        bpm_handler_base.__init__(self)
+
+    def set_attenuation(self,pv,value):
+        bpm_handler_base.bpm_control.setSA1(pv,value)
+        bpm_handler_base.bpm_control.setSA2(pv,value)
+        bpm_handler_base.logger.message('setting SA1 = SA2 = ' + str(value) + ' for ' + pv, True)
+
+    def read_attenuation(self, pv):
+        bpm_handler_base.data.values[dat.get_ra1] = bpm_handler_base.bpm_control.getRA1(pv)
+        bpm_handler_base.data.values[dat.get_ra2] = bpm_handler_base.bpm_control.getRA2(pv)
+        bpm_handler_base.logger.message('RA1 = ' + str(bpm_handler_base.data.values[dat.get_ra1]) + ' for ' + pv, True)
+        bpm_handler_base.logger.message('RA2 = ' + str(bpm_handler_base.data.values[dat.get_ra2]) + ' for ' + pv, True)
+
+    def set_delay(self, pv, value):
+        bpm_handler_base.bpm_control.setSD1(pv, value)
+        bpm_handler_base.bpm_control.setSD2(pv, value)
+        bpm_handler_base.logger.message('setting SD1 = SD2 = ' + str(value) + ' for ' + pv, True)
+
+    def set_dly_1(self, pv, value):
+        bpm_handler_base.bpm_control.setSD1(pv, value)
+        bpm_handler_base.logger.message('setting SD1 = ' + str(value) + ' for ' + pv, True)
+
+    def set_dly_2(self, pv, value):
+        bpm_handler_base.bpm_control.setSD2(pv, value)
+        bpm_handler_base.logger.message('setting SD2 = ' + str(value) + ' for ' + pv, True)
+
+    def read_delay(self, pv):
+        bpm_handler_base.data.values[dat.get_rd1] = bpm_handler_base.bpm_control.getRD1(pv)
+        bpm_handler_base.data.values[dat.get_rd2] = bpm_handler_base.bpm_control.getRD2(pv)
+        bpm_handler_base.logger.message('RD1 = ' + str(bpm_handler_base.data.values[dat.get_rd1]) + ' for ' + pv, True)
+        bpm_handler_base.logger.message('RD2 = ' + str(bpm_handler_base.data.values[dat.get_rd2]) + ' for ' + pv, True)
+
+    def update_bpm_voltages(self):
+        if bpm_handler_base.data.values[dat.bpm_status] and bpm_handler_base.data.values[dat.scope_status]:
+            bpm_handler_base.data.values[dat.bpm_raw_data_mean_v11] = abs(numpy.mean(bpm_handler_base.data.values[dat.bpm_u11]) - numpy.mean(bpm_handler_base.data.values[dat.bpm_u14]))
+            bpm_handler_base.data.values[dat.bpm_raw_data_mean_v12] = abs(numpy.mean(bpm_handler_base.data.values[dat.bpm_u12]) - numpy.mean(bpm_handler_base.data.values[dat.bpm_u14]))
+            bpm_handler_base.data.values[dat.bpm_raw_data_mean_v21] = abs(numpy.mean(bpm_handler_base.data.values[dat.bpm_u21]) - numpy.mean(bpm_handler_base.data.values[dat.bpm_u24]))
+            bpm_handler_base.data.values[dat.bpm_raw_data_mean_v22] = abs(numpy.mean(monitor.data.values[dat.bpm_u22]) - numpy.mean(monitor.data.values[dat.bpm_u24]))
+            bpm_handler_base.data.values[dat.bpm_v11_v12_sum][bpm_handler_base.data.values[dat.set_sa1_current]] = (bpm_handler_base.data.values[
+                                                                                                      dat.bpm_raw_data_mean_v11] +
+                                                                                                                    bpm_handler_base.data.values[
+                                                                                                      dat.bpm_raw_data_mean_v12]) / 2
+            bpm_handler_base.data.values[dat.bpm_v21_v22_sum][bpm_handler_base.data.values[dat.set_sa2_current]] = (bpm_handler_base.data.values[
+                                                                                                      dat.bpm_raw_data_mean_v21] +
+                                                                                                                    bpm_handler_base.data.values[
+                                                                                                      dat.bpm_raw_data_mean_v22]) / 2
+
+    def scan_dly1(self, dly_1):
+        self.set_dly_1(bpm_handler_base.data.values[dat.bpm_name],dly_1)
+        bpm_handler_base.data.values[dat.dv1_dly1][dly_1] = \
+            (2 * numpy.mean(bpm_handler_base.data.values[dat.bpm_u11]) ) / (
+                numpy.mean(bpm_handler_base.data.values[dat.bpm_u21]) + numpy.mean(bpm_handler_base.data.values[dat.bpm_u22]) )
+        bpm_handler_base.data.values[dat.dv2_dly1][dly_1] = \
+            (2 * numpy.mean(bpm_handler_base.data.values[dat.bpm_u12])) / (
+                numpy.mean(bpm_handler_base.data.values[dat.bpm_u21]) + numpy.mean(bpm_handler_base.data.values[dat.bpm_u22]))
+
+    def find_min_dly_1(self):
+        print bpm_handler_base.data.values[dat.dv1_dly1].values()
+        bpm_handler_base.data.values[dat.dv1_dly1_min_val] = min(bpm_handler_base.data.values[dat.dv1_dly1].values())
+        bpm_handler_base.data.values[dat.dv2_dly1_min_val] = min(bpm_handler_base.data.values[dat.dv2_dly1].values())
+        bpm_handler_base.data.values[dat.new_dly_1] = int(numpy.mean(bpm_handler_base.data.values[dat.dv1_dly1_min_val] + bpm_handler_base.data.values[dat.dv2_dly1_min_val]))
+        self.set_dly_1(bpm_handler_base.data.values[dat.bpm_name],bpm_handler_base.data.values[dat.new_dly_1])
+        self.read_delay(bpm_handler_base.data.values[dat.bpm_name])
+        bpm_handler_base.data.values[dat.new_dly_1_set] = True
+
+    def scan_dly2(self, dly_2):
+        self.set_dly_2(bpm_handler_base.data.values[dat.bpm_name], dly_2)
+        bpm_handler_base.data.values[dat.dv1_dly2][dly_2] = \
+            (2 * numpy.mean(bpm_handler_base.data.values[dat.bpm_u11]) ) / (
+                numpy.mean(bpm_handler_base.data.values[dat.bpm_u21]) + numpy.mean(bpm_handler_base.data.values[dat.bpm_u22]) )
+        bpm_handler_base.data.values[dat.dv2_dly2][dly_2] = \
+            (2 * numpy.mean(bpm_handler_base.data.values[dat.bpm_u12])) / (
+                numpy.mean(bpm_handler_base.data.values[dat.bpm_u21]) + numpy.mean(bpm_handler_base.data.values[dat.bpm_u22]))
+
+    def find_min_dly_2(self):
+        bpm_handler_base.data.values[dat.dv1_dly2_min_val] = min(bpm_handler_base.data.values[dat.dv1_dly2].values())
+        bpm_handler_base.data.values[dat.dv2_dly2_min_val] = min(bpm_handler_base.data.values[dat.dv2_dly2].values())
+        bpm_handler_base.data.values[dat.new_dly_2] = int(numpy.mean(bpm_handler_base.data.values[dat.dv1_dly2_min_val] + bpm_handler_base.data.values[dat.dv2_dly2_min_val]))
+        self.set_dly_1(bpm_handler_base.data.values[dat.bpm_name],bpm_handler_base.data.values[dat.new_dly_2])
+        self.read_delay(bpm_handler_base.data.values[dat.bpm_name])
+        bpm_handler_base.data.values[dat.new_dly_2_set] = True
---
+++ Apps/BPMCalibration/controllers/main_controller.py
@@ -0,0 +1,117 @@
+# THE main_controller
+# holds everything
+from PyQt4.QtGui import QApplication
+from controller_base import controller_base
+from gui.bpm_calibration_gui import bpm_calibration_gui
+import data.bpm_calibrate_data_base as dat
+import sys
+import time
+from timeit import default_timer as timer
+
+
+class main_controller(controller_base):
+    # whoami
+    my_name = 'main_controller'
+    #
+    # other attributes will be initiliased in base-class
+
+    def __init__(self, argv, config_file):
+        controller_base.__init__(self,argv, config_file)
+        # start monitoring data
+        self.data_monitor.start_monitors()
+        # build the gui and pass in the data
+        #
+        # build the gui
+        self.gui = bpm_calibration_gui()
+        self.gui.closing.connect(self.connectCloseEvents)
+        self.gui.show()
+        self.gui.activateWindow()
+        QApplication.processEvents()
+
+        # set up main_loop main states
+        self.monitor_states = self.data.main_monitor_states
+        QApplication.processEvents()
+
+        # start data recording
+        self.data.start_logging()
+        QApplication.processEvents()
+
+        # everything now runs from the main_loop
+        self.main_loop()
+
+    def main_loop(self):
+        self.logger.header(self.my_name + ' BPM attenuation calibration is entering main_loop !',True)
+
+        self.data_monitor.init_monitor_states()
+    #     #
+        time.sleep(1)
+        while 1:
+            while 1:
+                if controller_base.data.values[dat.ready_to_go]:
+                    self.clear_values()
+                    controller_base.data.values[dat.scan_status] = 'scanning'
+                    if controller_base.data.values[dat.calibration_type] == 'attenuation':
+                        self.set_attenuations_and_record()
+                        controller_base.data.values[dat.scan_status] = 'complete'
+                    elif controller_base.data.values[dat.calibration_type] == 'delay':
+                        self.set_delays_and_record()
+                        controller_base.data.values[dat.scan_status] = 'complete'
+                    break
+                else:
+                    QApplication.processEvents()
+
+            if controller_base.data.values[dat.calibration_type] == 'attenuation':
+                self.gui.plot_bpm_vs_sa()
+
+                if controller_base.data.values[dat.plots_done]:
+                    att1_v1_cal = controller_base.data_monitor.bpm_monitor.find_nearest(controller_base.data.values[dat.bpm_v11_v12_sum], 1.0)
+                    att2_v2_cal = controller_base.data_monitor.bpm_monitor.find_nearest(controller_base.data.values[dat.bpm_v21_v22_sum], 1.0)
+                    controller_base.data.values[dat.att_1_cal] = att1_v1_cal[0]
+                    controller_base.data.values[dat.v_1_cal] = att1_v1_cal[1]
+                    controller_base.data.values[dat.att_2_cal] = att2_v2_cal[0]
+                    controller_base.data.values[dat.v_2_cal] = att2_v2_cal[1]
+                    controller_base.data.values[dat.q_cal] = controller_base.data.values[dat.bunch_charge]
+
+            elif controller_base.data.values[dat.calibration_type] == 'delay':
+                self.gui.plot_bpm_vs_sd()
+
+            controller_base.data.values[dat.ready_to_go] = False
+
+    def set_attenuations_and_record(self):
+        if controller_base.data.values[dat.ready_to_go]:
+            for i in range(controller_base.data.values[dat.set_start], controller_base.data.values[dat.set_end]):
+                controller_base.bpm_handler.set_attenuation(controller_base.data.values[dat.bpm_name], i)
+                controller_base.data_monitor.bpm_monitor.update_bpm_attenuations()
+                if controller_base.data_monitor.bpm_monitor.check_sa_equals_ra():
+                    # controller_base.data_monitor.bpm_monitor.update_bpm_raw_data()
+                    controller_base.data_monitor.bpm_monitor.update_bpm_voltages()
+                    controller_base.data_monitor.scope_monitor.update_bunch_charge()
+
+    def set_delays_and_record(self):
+        if controller_base.data.values[dat.ready_to_go]:
+            for i in range(controller_base.data.values[dat.set_start], controller_base.data.values[dat.set_end]):
+                controller_base.data_monitor.bpm_monitor.update_bpm_voltages()
+                controller_base.bpm_handler.scan_dly1(i)
+                controller_base.data_monitor.scope_monitor.update_bunch_charge()
+            controller_base.bpm_handler.find_min_dly_1()
+            if controller_base.data.values[dat.new_dly_1_set]:
+                for i in range(controller_base.data.values[dat.new_dly_1] - 20, controller_base.data.values[dat.new_dly_1] + 20):
+                    controller_base.data_monitor.bpm_monitor.update_bpm_voltages()
+                    controller_base.bpm_handler.scan_dly2(i)
+                    controller_base.data_monitor.scope_monitor.update_bunch_charge()
+                controller_base.bpm_handler.find_min_dly_2()
+
+    def clear_values(self):
+        controller_base.data.values[dat.bpm_v11_v12_sum] = {}
+        controller_base.data.values[dat.bpm_v21_v22_sum] = {}
+
+    # over load close
+    def connectCloseEvents(self):
+        self.gui.close()
+        sys.exit()
+
+    def set_last_mask_epoch(self):
+        self.epoch = time.time()
+
+    def seconds_passed(self,secs):
+        return time.time() - self.epoch >= secs
