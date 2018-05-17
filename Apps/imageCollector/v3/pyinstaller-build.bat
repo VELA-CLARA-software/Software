6bc9d50a2fbda8c48ae0950f25bbf2e11d58fe7e
Duncan <duncan.scott@stfc.ac.uk>
2018-05-17 15:46:58 +0100

    fixed load file bug for clara_2_BA1

--- Apps/imageCollector/v3/pyinstaller-build.bat
+++ Apps/imageCollector/v3/pyinstaller-build.bat
@@ -1,2 +1,2 @@
 rem Compile the app to an EXE file
-pyinstaller --onefile -i view\icon.ico -n "imageCollector" mainApp.py
+pyinstaller -F -w --icon=.\resources\imageCollector\eye.ico -n "imageCollector" mainApp.py
