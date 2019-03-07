95e6ecab537eed21be1dd62000b1e6d8e9997d2f
Ben Shepherd <bjashepherd@gmail.com>
2019-03-07 08:44:01 +0000

    [undulator_scan] tidied up, added comments

--- Procedures/undulator_scan/undulator_scan.py
+++ Procedures/undulator_scan/undulator_scan.py
@@ -1,3 +1,12 @@
+#!python2
+# -*- coding: utf-8 -*-
+# encoding=utf8
+
+# Scan a corrector (or any EPICS PV) and read a measurement from a networked scope
+# Ben Shepherd, James Jones
+# 6 March 2019
+# Originally written to optimise the MCT signal output from the SCU installed on CLARA
+
 import sys
 import os
 import time
@@ -7,37 +16,47 @@ import urllib2
 sys.path.append("../../../")
 import Software.Utils.dict_to_h5 as h5dict
 
+
+work_folder = r'\\fed.cclrc.ac.uk\Org\NLab\ASTeC\Projects\VELA\Work'
 data = {}
 data['corrector'] = 'CLA-C2V-MAG-VCOR-01'
 data['Undulator_Current'] = 120
-vcor = epics.PV(data['corrector']+':SETI')
-start_current = vcor.get()
-min_v, max_v = -5.0, 5.0
+scan_pv = epics.PV(data['corrector'] + ':SETI')
+start_value = scan_pv.get()
+min_val, max_val = -5.0, 5.0
 step = 0.2
-url = 'http://148.79.170.74/data/comm.html?command=:MEASU:MEAS1:VAL?'
+scope_url = 'http://148.79.170.74/data/comm.html?command=:MEASU:MEAS1:VAL?'  # asks for the value of measurement 1
+wait_time = 6.4  # if scope averaging is enabled, need to wait before asking for a measurement
 
-i_range = np.arange(min_v, max_v + step, step)
+intro_text = 'Scanning {pv_name} from {min_val:.3f} {units} to {max_val:.3f} {units} in steps of {step:.3f} {units}' \
+             ' with a delay of {wait_time} s at each step. Original value was {start_value:.3f} {units}.'
+print(intro_text.format(pv_name=data['corrector'], units=scan_pv.units, **locals()))
+
+scan_range = np.arange(min_val, max_val + step, step)
 signal_array = []
-for i, current in enumerate(i_range):
+for i, current in enumerate(scan_range):
     # print(current)
-    vcor.put(current)
-    # sleep(6.4)
-    time.sleep(6.4)
+    scan_pv.put(current)
+    time.sleep(wait_time)
 
-    contents = urllib2.urlopen(url).read()
+    contents = urllib2.urlopen(scope_url).read()  # for example scope response, see scope_response.htm
+    # first line is semicolon-delimited list of measurement results - we can discard the other lines
     lines = contents.split('\n')
     signal = float(lines[0])
-    signal_array.append([current,signal])
+    signal_array.append([current, signal])
     print current, signal
 
-vcor.put(start_current)
-data['data'] = signal_array
+print('Scan complete. Restoring {pv_name} to {start_value:.3f} {units}.'.format(pv_name=data['corrector'], **locals()))
+scan_pv.put(start_value)  # restore the original value
+data['data'] = signal_array  # store 2d array in HDF5 file - each row is (current, signal) pair
 timestr = time.strftime("%H%M%S")
-dir = '\\\\fed.cclrc.ac.uk\\Org\\NLab\\ASTeC\\Projects\\VELA\\Work\\'+time.strftime("%Y\\%m\\%d")+'\\'
+folder = work_folder + '\\' + time.strftime(r"%Y\%m\%d") + '\\'
 try:
-    os.makedirs(dir)
+    os.makedirs(folder)
 except OSError:
-    if not os.path.isdir(dir):
-        dir = './'
-filename = dir+timestr+'_UndulatorScans_'+str(data['Undulator_Current'])+'A_'+data['corrector']+'.h5'
+    if not os.path.isdir(folder):
+        folder = './'
+
+filename = folder + timestr + '_UndulatorScans_' + str(data['Undulator_Current']) + 'A_' + data['corrector'] + '.h5'
+print(' Saving data to {}.'.format(filename))
 h5dict.save_dict_to_hdf5(data, filename)
