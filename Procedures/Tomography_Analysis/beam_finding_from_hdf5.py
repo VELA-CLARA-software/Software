b546ce21b8eb741af9d35d17d4cc5d05c57e167a
Duncan <duncan.scott@stfc.ac.uk>
2019-08-30 14:21:41 +0100

    beam centre and cropping working

--- Procedures/Tomography_Analysis/beam_finding_from_hdf5.py
+++ Procedures/Tomography_Analysis/beam_finding_from_hdf5.py
@@ -20,6 +20,7 @@ def get_first_second_moments(data):
     moment_2 = np.sqrt(np.dot((data_indices - moment_1) ** 2, data) / normalisation)
     return [moment_1, moment_2]
 
+
 def normal_dist(x, B, A, mean, sigma):
     '''
     A normal distribution
@@ -30,7 +31,7 @@ def normal_dist(x, B, A, mean, sigma):
     :param sigma:
     :return: value at x
     '''
-    return B + A * exp(-(x-mean)**2/(2*sigma**2))
+    return B + A * exp(-(x - mean) ** 2 / (2 * sigma ** 2))
 
 
 def fit_normal(data):
@@ -45,24 +46,28 @@ def fit_normal(data):
     B = 0
     A = max(y)
     paramater_guess = [B, A, mean, sigma]
-    popt,pcov = curve_fit(f=normal_dist, xdata=x, ydata=y, p0 = paramater_guess)
+    popt, pcov = curve_fit(f=normal_dist, xdata=x, ydata=y, p0=paramater_guess)
     return popt
 
 
-def plot_normal_fit(data1, popt1, data2, popt2):
+def plots(data1, popt1, data2, popt2, final_image):
     fig = plt.figure(figsize=(17, 8))
-    ax = fig.add_subplot(121)
-    ay = fig.add_subplot(122)
+    ax = fig.add_subplot(131)
+    ay = fig.add_subplot(132)
+    az = fig.add_subplot(133)
     x1 = np.array(range(len(data1)), dtype=np.float32)
     y1 = np.array(data1, dtype=np.float32)
 
     x2 = np.array(range(len(data2)), dtype=np.float32)
     y2 = np.array(data2, dtype=np.float32)
 
-    ax.plot(x1,y1,'b+:',label='data')
-    ax.plot(x1,normal_dist(x1,*popt1),'ro:',label='fit')
-    ay.plot(x2,y2,'b+:',label='data')
-    ay.plot(x2,normal_dist(x2,*popt2),'ro:',label='fit')
+    ax.plot(x1, y1, 'b+:', label='data')
+    ax.plot(x1, normal_dist(x1, *popt1), 'ro:', label='fit')
+    ay.plot(x2, y2, 'b+:', label='data')
+    ay.plot(x2, normal_dist(x2, *popt2), 'ro:', label='fit')
+
+    az.imshow(final_image)
+
     ax.legend()
     plt.xlabel('X (PIXEL)')
     plt.ylabel('y (PIXEL)')
@@ -86,44 +91,87 @@ def get_image_data_to_fit(beam_fn, back_fn):
     # all images in the hdf5 file have this string in their 'group' (key)
     image_tag = 'Capture'
     # get all keys that have image_tag in them
-    beam_keys = [x for x in beam_image.keys() if image_tag in x ]
-    back_keys = [x for x in back_image.keys() if image_tag in x ]
+    beam_keys = [x for x in beam_image.keys() if image_tag in x]
+    back_keys = [x for x in back_image.keys() if image_tag in x]
     # now we can find an average of all the beam / background images
     # get the first beam image
-    print "getting beam images"
-    beam_0 = np.array(back_image[back_keys[0]][()],dtype=np.float32)
+    #print "getting beam images"
+    beam_0 = np.array(back_image[back_keys[0]][()], dtype=np.float32)
     for image in beam_keys[1:]:
-        temp = np.array(beam_image[image][()],dtype=np.float32)
+        temp = np.array(beam_image[image][()], dtype=np.float32)
         beam_0 = np.add(temp, beam_0)
-        print beam_0[0]
-    print("len(beam_keys)",len(beam_keys))
+        #print beam_0[0]
+    #print("len(beam_keys)", len(beam_keys))
     beam_0 = np.true_divide(beam_0, len(beam_keys))
-    print beam_0[0]
+    #print beam_0[0]
 
-    print "getting back images"
-    back_0 = np.array(back_image[back_keys[0]][()],dtype=np.float32)
+    #print "getting back images"
+    back_0 = np.array(back_image[back_keys[0]][()], dtype=np.float32)
     for image in back_keys[1:]:
-        temp = np.array(back_image[image][()],dtype=np.float32)
+        temp = np.array(back_image[image][()], dtype=np.float32)
         back_0 = np.add(temp, back_0)
-        print back_0[0]
-    back_0 = np.true_divide(back_0 , len(back_keys))
-    print back_0[0]
+        #print back_0[0]
+    back_0 = np.true_divide(back_0, len(back_keys))
+    #print back_0[0]
 
-    beam_image = np.subtract(beam_0,back_0)
+    beam_image = np.subtract(beam_0, back_0)
     # slice first 50 columns out, as this is where the time stamp is imprinted in the image
-    return beam_image  [..., 49:]
+    return beam_image[..., 49:]
+
+
+def get_beam_centered_image(input_image, col_center, row_center, return_width, return_height):
+    '''
+    takes imaged_data and crops it around row_centre, and col_centre to give an image of
+    return_width and return_height pixels
+    TODO: add exceptions if we are cropping outside the input_image
+    :param input_image: data to crop
+    :param col_center: central column of cropped image
+    :param row_center:  central row of cropped image
+    :param return_width: cropped image width
+    :param return_height: cropped image height
+    :return: cropped image
+    '''
+
+    half_return_width = return_width * 0.5
+    half_return_height= return_height * 0.5
+
+    start_col = int(round(col_center - half_return_width))
+    start_row = int(round(row_center - half_return_height))
+    end_col = int(round(col_center + half_return_width))
+    end_row = int(round(row_center + half_return_height))
+
+    # check we're not cropping outside the original image
+
+    if start_col < 0:
+        start_col = 0
+        print("Warning! start_col < 0")
+    if start_row < 0:
+        print("Warning! start_row < 0")
+        start_row = 0
+
+    if end_col >= len(input_image[0]):
+        print("Warning! end_col >= " + str(len(input_image[0])))
+        end_col = len(input_image[0]) - 1
+    if end_row < 0:
+        print("Warning! end_row >= " + str(len(input_image)))
+        end_row = len(input_image) - 1
+
+    return input_image[start_row:end_row, start_col:end_col]
 
 
 
+#
+#   RUNNING
+#
 # Some inputs
 
+
 # where are the raw image files we want to analyse?
-working_directory='\\\\fed.cclrc.ac.uk\\org\\NLab\\ASTeC\\Projects\\VELA\\Work\\2019\\03\\04' \
-                  '\\10pC degaussing, sol -125.0'
+working_directory = '\\\\fed.cclrc.ac.uk\\org\\NLab\\ASTeC\\Projects\\VELA\\Work\\2019\\03\\04' \
+                    '\\10pC degaussing, sol -125.0'
 
 # ImageFileInformation contains a list fo which beam image goes with which background image
 image_file_info_path = os.path.join(working_directory, 'ImageFileInformation.mat')
-
 
 # get the image file information (beam images and background)
 image_file_information = loadmat(image_file_info_path)
@@ -133,13 +181,11 @@ image_files = 'Image_and_background_filenames_at_observation_point'
 # there can be multiple beam images and multiple background images
 # we sum all teh images, find an average, then subtract the background from the beam
 
-
 # filenames for the data to read
-beam_fn = os.path.join(working_directory, str(image_file_information[image_files][0,0][0]) )
-back_fn = os.path.join(working_directory, str(image_file_information[image_files][0,1][0]) )
+beam_fn = os.path.join(working_directory, str(image_file_information[image_files][0, 0][0]))
+back_fn = os.path.join(working_directory, str(image_file_information[image_files][0, 1][0]))
 
-
-image_to_fit = get_image_data_to_fit( beam_fn, back_fn)
+image_to_fit = get_image_data_to_fit(beam_fn, back_fn)
 print image_to_fit[0]
 
 # Now we fit a normal distribution to the projections of the image_to_fit
@@ -148,16 +194,20 @@ print image_to_fit[0]
 sum_cols = image_to_fit.sum(axis=0).tolist()
 sum_rows = image_to_fit.sum(axis=1).tolist()
 
-
 fit_cols = fit_normal(sum_cols)
 fit_rows = fit_normal(sum_rows)
 
-plot_normal_fit(sum_cols,fit_cols, sum_rows,fit_rows)
 
+# now we crop image_to_fit based on teh beam center
+final_image = get_beam_centered_image(input_image=image_to_fit, col_center=fit_cols[2],
+                                      row_center=fit_rows[2], return_width=1200, return_height=1200)
+
+
+
+plots(sum_cols, fit_cols, sum_rows, fit_rows, final_image)
 
 # now we can crop the data about the centre of each gaussian
 
-final_image_size = 1200 # pixels
-
+final_image_size = 1200  # pixels
 
 raw_input()
