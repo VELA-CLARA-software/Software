a02bb2b5833a487c55f625323e9c5f089a18b93d
Hannah Kockelbergh <hannah.kockelbergh@stfc.ac.uk>
2019-09-17 11:50:03 +0100

    Update ScaleImages.py
    images rotated 90 degrees initially to produce same result as matlab image scaling

--- Procedures/Tomography_Analysis/ScaleImages.py
+++ Procedures/Tomography_Analysis/ScaleImages.py
@@ -2,6 +2,7 @@ import scipy.io as sio
 import numpy as np
 import cv2
 import math
+import matplotlib.pyplot as plt
 
 # define some parameters
 magnfcnx=10
@@ -14,28 +15,37 @@ images=beamimages['imagearray']
 images=images[0]
 
 # load beta at observation point for each image
-imagefileinformation=sio.loadmat('ImageFileInformation.mat')
-betaobs=imagefileinformation['Beta_x_y_at_observation_point']
+beta=sio.loadmat('BetaObs.mat')
+betaobs=beta['Beta_x_y_at_observation_point']
 betaxobs=betaobs[:,0]
 betayobs=betaobs[:,1]
 
 # initalise image array
+
 imagearray=np.empty((len(images),psresn,psresn))
 
+plt.figure()
+r = 0.5
+nmax=len(images)
+num_rows = math.floor(r*math.sqrt(nmax/r))
+num_cols = math.ceil(nmax/num_rows)
 # iterate through images
 for image, i in zip(images, range(len(images))):
-
+    image=np.rot90(image)
     # scale each image
-    imresized=cv2.resize(image,None,fx =magnfcnx/np.sqrt(betaxobs[i]),fy = magnfcny/np.sqrt(betayobs[i]),interpolation = cv2.INTER_LINEAR)
+    imresized=cv2.resize(image,None,fx =magnfcnx/math.sqrt((betaxobs[i])),fy = magnfcny/(math.sqrt(betayobs[i])),interpolation = cv2.INTER_CUBIC)
 
     # initialise imscaled
     imscaled = np.zeros_like(image)
 
+
+    def imagecentre(image):
+        centre = (np.floor(np.asarray(np.shape(image)) + 1) / 2).astype(int)
+        return centre[0],centre[1]
+
     # original and scaled image centres, correct to 1 pixel
-    x1 = math.floor((np.shape(imresized)[0]+1)/2)
-    y1 = math.floor((np.shape(imresized)[1]+1)/2)
-    x2 = math.floor((np.shape(imscaled)[0]+1)/2)
-    y2 = math.floor((np.shape(imscaled)[1]+1)/2)
+    x1, y1 = imagecentre(imresized)
+    x2, y2 = imagecentre(imscaled)
 
     # get smallest of each dimension
     smallestx = min(x1,x2)-1
@@ -45,10 +55,15 @@ for image, i in zip(images, range(len(images))):
     imscaled[(x2-smallestx):(x2+smallestx),(y2-smallesty):(y2+smallesty)] = imresized[(x1-smallestx):(x1+smallestx),(y1-smallesty):(y1+smallesty)]
 
     # resize again to fit pixel resolution
-    imrescaled=cv2.resize(imscaled,(psresn,psresn),interpolation = cv2.INTER_LINEAR)
+    imrescaled=cv2.resize(imscaled,(psresn,psresn),interpolation = cv2.INTER_CUBIC)
 
     # normalise by total intensity
     imnormed=imrescaled/np.sum(imrescaled)
 
     # store in image array
     imagearray[i]=imnormed
+
+    plt.subplot(num_rows, num_cols,i+1)
+    plt.imshow(imnormed)
+plt.show()
+
