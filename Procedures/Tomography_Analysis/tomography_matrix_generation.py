40f88d981b7248c203b8d76d5e7e60cbbafd90bd
Duncan <duncan.scott@stfc.ac.uk>
2019-10-16 14:07:49 +0100

    Tomography matrix generation and phase space estimation (neatened)

--- Procedures/Tomography_Analysis/tomography_matrix_generation.py
+++ Procedures/Tomography_Analysis/tomography_matrix_generation.py
@@ -1,17 +1,42 @@
 import numpy as np
 import h5py
 from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import lsqr as lsqr
+from scipy.io import loadmat
+
 '''
-This is the first attempt at following the matlab script, to be improved when it is working ... 
+This is a working copy of the matlab script, to do the tomography analysis 
+It uses CLARATomography.m and covers teh code from after the image's have been prepared / scaled 
+etc. 
+It DOES NOT YET create the final phase space plots  
+First we build the "Tomography Matrix" based on pre-computed phase advances
+for each quad setting 
+
+The we build the "tomography matrix" which part of the projection tells where the beam in pixel in 
+phase space
+"ends up"  
+
+Then we use it with the bream projection data to find a phase space distribution that 
+re-creates "as good a a match" to the projections as we can.  
+
+
+!!!!!!!!WARNING!!!!!!!! 
+Throughout this code there is the possibility of getting the array indexes wrong. In 
+the original Matlab code the first elements have index 1, in python the first elements are index 0 
+As is common with these types of calculation it can be very easy to be off by a single pixel
+What is presented here exactly reproduces the Matlab output 
+
 '''
 
+'''
+First we define some constants, and input data,
+I expect that these numbers will be defined / calculated earlier in the procedure 
+'''
 
-#
-# Build the Tomography Matrix ,
-# we're going to try adn reproduce this code from the matlab script
-
-# phase advances (between obs and recon point?for each quad setting,
-# These are calcaulted in an ealrier part of the procedure
+'''
+phase advances (between observation  and recon point?for each quad setting,
+These are calculated earlier in the procedure
+'''
 phase_advances = [[0.336646899467633, 0.228677801765683], [0.187706900011030, 0.246774303860592],
                   [0.557301245734776, 0.218226902788538], [0.646249817696088, 0.212050203302031],
                   [0.847182618687409, 0.251044121023979], [0.110780647521904, 0.468106739372366],
@@ -31,272 +56,201 @@ phase_advances = [[0.336646899467633, 0.228677801765683], [0.187706900011030, 0.
                   [2.86285656512334, 1.61993600234020], [1.80958014192688, 3.16447504868879],
                   [2.11112675621833, 3.22156906985141], [3.17202280936962, 0.514232105089073],
                   [2.85305455690855, 0.890605054554737], [2.10827467056920, 3.22377151541008]]
+'''
+Cherry pick our datasets.
 
-# These are which data (by index) we are going to use for analysis I presume they are chosen by 
-# hand ... ?
-indxx = [1, 2, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28,
-         29, 30, 32, 33, 34, 35, 36, 37, 38]
-indxx_py  =[x - 1 for x in indxx]
-nx = len(indxx)
-indxy = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-         26, 27, 28, 29, 30, 31, 32, 34, 35, 36, 37]
-indxy_py  =[x - 1 for x in indxy]
-ny = len(indxx)
+Here we are manually choosing which quad settings (by index) to include in the tomogrpahy 
+calculation,
 
-# the processed image resolution? number of pixels in the formatted data?
+How do we choose? witchcraft - i've copied this from the original matlab script, Incoporating 
+this step into our analysis will not be trivial   
+'''
+xdata_matlab = [1, 2, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+                27, 28, 29, 30, 32, 33, 34, 35, 36, 37, 38]
+xdata_indexes = [x - 1 for x in xdata_matlab]
+nx = len(xdata_indexes)
+
+ydata_matlab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+                24, 25, 26, 27, 28, 29, 30, 31, 32, 34, 35, 36, 37]
+ydata_indexes = [x - 1 for x in ydata_matlab]
+ny = len(ydata_indexes)
+
+print('Number of x datasets = ', nx)
+print('Number of y datasets = ', ny)
+
+'''
+constants for processed image resolution 
+the processed image resolution, or number of pixels in x and y for the processed images 
+This is the offset to the centre of the beam. The processed images are all centered on the
+electron beam ...
+'''
 psresn = 201
-# This is the offset to thr centre of the beam? the processed images are all centered on the
-# electron beam ...
 ctroffset = 101
 
-
-
-
-
-# load in image array, this is an array of rescaled images for each quad setting,
+'''
+load in processed image array, this is an array of rescaled images for each quad setting,
+Ths is calculated earlier in the procedure, here we are importing the Matlab results
+'''
 imagearraypath = ".//Image Scaling//matlab_imagearray.h5"
 image_array_file = h5py.File(imagearraypath, 'r+')
-image_array = image_array_file["/data" ]
-#print(image_array[0].sum(axis=0))
+image_array = image_array_file["/data"]
 
+'''
+Create empty arrays for results
 
+dfindxx, dfindxy are the "Tomography Matrices"
 
+xprojection, yprojection are the beam image projections 
 
-# np.zeros((2, 1))
-# array([[ 0.],
-#        [ 0.]])
-# IN MATLAB!!
-# zeros([2 3]) returns a 2-by-3 matrix.
+!!!!WARNING!!!! SET THE CORRECT TYPE HERE
 
-# fill empty vectors for this (needs a better name)
-dfindxx = np.zeros(( 2,nx*pow(psresn,2)),dtype=int)
-dfindxy = np.zeros(( 2,ny*pow(psresn,2)),dtype=int)
+'''
 
+dfindxx = np.zeros((2, nx * pow(psresn, 2)), dtype=int)
+dfindxy = np.zeros((2, ny * pow(psresn, 2)), dtype=int)
+print('dfindxx shape =  ', dfindxx.shape)
+print('dfindxy shape =  ', dfindxy.shape)
 
-print('dfindxx shape =  ', dfindxx.shape )
-print('dfindxy shape =  ', dfindxy.shape )
-
-
-# fill empty vectors for projections
-# SET THE CORRECT TYPE HERE!!!
 xprojection = np.zeros((nx * psresn), dtype=float)
 yprojection = np.zeros((ny * psresn), dtype=float)
+print('xprojections length = ', len(xprojection))
+print('yprojections length = ', len(yprojection))
 
-#xprojection[0:201] = np.array(image_array[0].sum(axis=0))
-#print(xprojection[0:201])
-
-# get 'Tomography mMatrix'
 '''
+Calculate the Tomography Matrix for X-phase space  
 loop over each x and px pixel
 I think what we are doing is: seeing if a pixel at the observation point, "maps" to a pixel in 
 the reconstruction point. (clearly all the pixels do, maybe i should say "is in the field of 
 view of the recon point?") There is probably a much more accurate wording for this  
+
+These loops can be turned in to a function, and probably calculated for all quad settings, 
+then you manually choose which dataset to leave out later ...  
 '''
-#I think this is just a counter to let you know which pixel in the recon part
-dfcntrx = 1
-for index in range(0, nx): # !!!!!!!!! the matlab code starts at 1
-    #print("index = ", index)
-    dataset = indxx_py[index ]
-
-    #print(" phase_advances = ", phase_advances[dataset][0])
-    #print(" phase_advances = ", phase_advances[dataset][0])
-
-    cosmux = np.cos( phase_advances[dataset][0])
-    sinmux = np.sin( phase_advances[dataset][0])
-    #print(" cosmux,sinmux = ", cosmux,sinmux)
-
-    # copying matlab code so loop indexes start at 1 ...
-    for x in range(1,psresn+1):
-        for px in range(1,psresn+1):
-            xindx0 = int(round(cosmux*(x - ctroffset) - sinmux*(px - ctroffset) + ctroffset))
-            #print('xindx0 = ', xindx0)
-            if 0 < xindx0:
-                if xindx0 <= psresn:
-                    pxindx0 = int(round(sinmux*(x-ctroffset) + cosmux*(px - ctroffset) + ctroffset))
-                    #print('pxindx0 = ', pxindx0)
-                    if 0 < pxindx0:
-                        if pxindx0 <= psresn:
-
-                            # different to matlba code as here 'index' starts at 0
-                            dfindxx[:,dfcntrx-1] = [(index)*psresn + x, (xindx0 - 1) *
-                             psresn+pxindx0]
-
-                            # print("[(index)*psresn + x, (xindx0 - 1) * psresn+pxindx0] = ",
-                            #       [(index)*psresn + x, (xindx0 - 1) * psresn+pxindx0])
-                            # print("dfindxx[dfcntrx] = ", dfindxx[dfcntrx])
-
-                            dfcntrx += 1
-
-                            # if dfcntrx % 10000 == 0:
-                            #     print(xindx0,pxindx0)
-
-
+print('Calculating (x, px) plane Tomography Matrix')
+count = 1  # a counter to let you know which pixel in the recon part
+for index in range(0, nx):  # !!!!!!!!! the matlab code starts at 1
+    # print("index = ", index)
+    dataset = xdata_indexes[index]
+    cosmux = np.cos(phase_advances[dataset][0])
+    sinmux = np.sin(phase_advances[dataset][0])
+    # print(" phase_advances = ", phase_advances[dataset][0])
+    # print(" cosmux,sinmux = ", cosmux,sinmux)
+    for x in range(1, psresn + 1):  # copying Matlab code so loop indexes start at 1 ...
+        for px in range(1, psresn + 1):
+            xindx0 = int(round(cosmux * (x - ctroffset) - sinmux * (px - ctroffset) + ctroffset))
+            if 0 < xindx0 and xindx0 <= psresn:
+                pxindx0 = int(
+                    round(sinmux * (x - ctroffset) + cosmux * (px - ctroffset) + ctroffset))
+                if 0 < pxindx0 and pxindx0 <= psresn:
+                    # Slight difference to Matlab as 'index' should start at 0 in Python
+                    dfindxx[:, count - 1] = [(index) * psresn + x, (xindx0 - 1) * psresn + pxindx0]
+                    # print("[(index)*psresn + x, (xindx0 - 1) * psresn+pxindx0] = ",
+                    #       [(index)*psresn + x, (xindx0 - 1) * psresn+pxindx0])
+                    # print("dfindxx[dfcntrx] = ", dfindxx[dfcntrx])
+                    count += 1  # increment counter  # if dfcntrx % 10000 == 0:  #     print(  #
+                    # xindx0,pxindx0)
     # x projection is sum of columns
-    xprojection[index*psresn : (index +1) * psresn ] = image_array[dataset].sum(axis=0)
+    xprojection[index * psresn: (index + 1) * psresn] = image_array[dataset].sum(axis=0)
+x_count_final = count
 
-    #print('dfcntrx = ', dfcntrx)
-    #print(xprojection[index*psresn] , "  ", (image_array[dataset].sum(axis=0))[0] )
-    #print("index = ", [index*psresn , (index +1) * psresn  ] )
-    #print("lens  = ", len(xprojection[index*psresn : (index +1) * psresn ]))
+print('Calculating (y, py) plane Tomography Matrix')
+count = 1  # a counter to let you know which pixel in the recon part
+for index in range(0, ny):  # !!!!!!!!! the matlab code starts at 1
+    # print("index = ", index)
+    dataset = ydata_indexes[index]
+    cosmuy = np.cos(phase_advances[dataset][1])
+    sinmuy = np.sin(phase_advances[dataset][1])
+    # print(" phase_advances = ", phase_advances[dataset][0])
+    # print(" cosmuy,sinmuy = ", cosmuy,sinmuy)
+    for y in range(1, psresn + 1):  # copying Matlab code so loop indexes start at 1 ...
+        for py in range(1, psresn + 1):
+            yindx0 = int(round(cosmuy * (y - ctroffset) - sinmuy * (py - ctroffset) + ctroffset))
+            if 0 < yindx0 and yindx0 <= psresn:
+                pyindx0 = int(
+                    round(sinmuy * (y - ctroffset) + cosmuy * (py - ctroffset) + ctroffset))
+                if 0 < pyindx0 and pyindx0 <= psresn:
+                    # Slight difference to Matlab as 'index' should start at 0 in Python
+                    dfindxy[:, count - 1] = [(index) * psresn + y, (yindx0 - 1) * psresn + pyindx0]
+                    count += 1  # increment counter
+    # x projection is sum of columns
+    yprojection[index * psresn: (index + 1) * psresn] = image_array[dataset].sum(axis=1)
+y_count_final = count
 
-
-from scipy.io import loadmat
-import os
-
-# where are the raw image files we want to analyse?
-
-#final count for dfcntrx
-print('Final dfcntrx = ', dfcntrx)
-print("xprojection[1-1] = ",xprojection[1-1])
-print("xprojection[1000-1] = ",xprojection[1000-1])
-print("xprojection[-1] = ",xprojection[-1])
-
-#% Finally, construct the (sparse) matrix relating the projected
-#% distribution at YAG02 to the phase space distribution at YAG01
-
-print('dfindxx[0:10] ',dfindxx[0:10]  )
-
-
-print("test format")
-print("dfindxx.shape = ", dfindxx.shape)
-dfindxx2 = dfindxx[:,0:dfcntrx-1] -1 # go from nmatlab 1 is first element to python 0 is first
-# element
-print("dfindxx2.shape = ", dfindxx2.shape)
-
-
-#findxx_mat_path = os.path.join(working_directory, 'dfindxx.mat')
-dfindxx_mat_data = loadmat('.//TomographyMatrix_djs//dfindxx.mat')
-dfindxx_mat = dfindxx_mat_data['dfindxx']
-print("dfindxx_mat.shape = ", dfindxx_mat.shape)
-
-print(dfindxx_mat[0])
-print(dfindxx_mat[1])
-print(dfindxx2[0])
-print(dfindxx2[1])
-print(dfindxx2 == dfindxx_mat )
-
-
-print("Create final sparse matrix")
-print("dfindxx2[0,:].shape = ",dfindxx2[0,:].shape)
-print("dfindxx2[1,:].shape = ",dfindxx2[1,:].shape)
-print(nx*psresn,psresn*psresn)
-
-# sparse matrix part
-# https://stackoverflow.com/questions/40890960/numpy-scipy-equivalent-of-matlabs-sparse-function
-# there are sparse ROW and sparse COLUMN matrices that are faster for operations on rows or columns
-# lets try a "compressed sparse row => csr"
+print('Building sparse matrices and solving')
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_matrix.html#scipy.sparse.csr_matrix
+dfindxx2 = dfindxx[:, 0:x_count_final - 1] - 1  # go from nmatlab 1 is first element to python 0
+dfullx = csr_matrix((np.ones(x_count_final - 1), (dfindxx2[0, :], dfindxx2[1, :])),
+                    shape=(nx * psresn, psresn * psresn))
+dfindxy2 = dfindxy[:, 0:y_count_final - 1] - 1  # go from nmatlab 1 is first element to python 0
+dfully = csr_matrix((np.ones(y_count_final - 1), (dfindxy2[0, :], dfindxy2[1, :])),
+                    shape=(ny * psresn, psresn * psresn))
 
+rhovectorx, istopx, itnx, normrx = lsqr(A=dfullx, b=xprojection, atol=0.000001, iter_lim=400)[:4]
+rhovectory, istopy, itny, normry = lsqr(A=dfully, b=yprojection, atol=0.000001, iter_lim=400)[:4]
 
-# i am not creating the sparse matrix correctly ....
-print("max(dfindxx2[0,:]) ", max(dfindxx2[0, :])  )
-print("max(dfindxx2[1,:]) ", max(dfindxx2[1, :])  )
+print("Complete")
+print("")
 
-
-dfullx = csr_matrix( (np.ones(dfcntrx-1), (dfindxx2[0,:], dfindxx2[1,:])), shape=(nx*psresn,
-                                                                              psresn*psresn))
-#dfully = sparse( dfindxy(1,:),dfindxy(2,:), ones(1,dfcntry-1), ny*psresn,psresn^2);
-
-
-from scipy.sparse.linalg import lsqr as lsqr
-rhovectorx, istop, itn, normr = lsqr( A=dfullx, b = xprojection, atol=0.000001, iter_lim =400)[:4]
-
-
-print("rhovectorx     ", rhovectorx  )
-print("istop ", istop  )
-print("itn   ", itn )
-print("normr ", normr )
-
-
-
-# Matlab answer
+'''
+Check some of the calculated numbers against matlab 
+'''
+# Load Matlab answers
 mat_datax = loadmat('.//TomographyMatrix_djs//TomographyMatrixX.mat')
 rhovectorx_mat = mat_datax['rhovectorx']
 xprojection_mat = mat_datax['xprojection']
 dfullx_mat = mat_datax['dfullx']
-
-print("xprojection     = ", xprojection)
-print("xprojection_mat = ", xprojection_mat )
-
-print("xprojection = ", xprojection == xprojection_mat.flatten() )
-print("xprojection = ", np.subtract(xprojection, xprojection_mat.flatten()) )
-print("rhovectorx = ", rhovectorx_mat == rhovectorx )
-#print("dfullx = ", dfullx_mat != dfullx )
-
 
 mat_datay = loadmat('.//TomographyMatrix_djs//TomographyMatrixY.mat')
 rhovectory_mat = mat_datay['rhovectory']
 yprojection_mat = mat_datay['yprojection']
 dfully_mat = mat_datay['dfully']
 
+print("Comparison With MATLAB results ")
+print("")
+print("X PROJECTION")
+print("xprojection     = ", xprojection)
+print("xprojection_mat = ", xprojection_mat.flatten())
+print("EQUAL ? = ", xprojection == xprojection_mat.flatten())
+print("rhovectorx     = ", rhovectorx)
+print("rhovectorx_mat = ", rhovectorx_mat.flatten())
+print("EQUAL ? = ", rhovectorx_mat == rhovectorx)
+print("")
+print("Y PROJECTION")
+print("yprojection     = ", yprojection)
+print("yprojection_mat = ", yprojection_mat.flatten())
+print("EQUAL ? = ", yprojection == yprojection_mat.flatten())
+print("rhovectory     = ", rhovectory)
+print("rhovectory_mat = ", rhovectory_mat.flatten())
+print("EQUAL ? = ", rhovectory_mat == rhovectory)
 
-rhox = rhovectorx.reshape((psresn,psresn))
-rhoy = rhovectory_mat.reshape((psresn, psresn))
-
-
-
-
+''' 
+    reproduce the plots  of projections and fitted projections / residuals
+'''
 print("PLOTTING ")
 
-# reproduce the plots
 import matplotlib.pyplot as plt
 
-fig = plt.figure(figsize=(17, 8))
-ax = fig.add_subplot(2,2,1)
-ax.plot(xprojection,'-k')
-ax.plot(dfullx*rhovectorx,'--r')
+fig = plt.figure(figsize=(15, 5))
+x1 = fig.add_subplot(2, 2, 1)
+x1.plot(xprojection, '-k')
+x1.plot(dfullx * rhovectorx, '--r')
+x2 = fig.add_subplot(2, 2, 2)
+x2.plot(xprojection - dfullx * rhovectorx, '-r')
+x2.set_xlabel('Data point')
+x2.set_ylabel('x residual')
 
-
-p2 = fig.add_subplot(2,2,2)
-p2.plot(xprojection - dfullx*rhovectorx,'-r')
-
-p2.set_xlabel('Data point')
-p2.set_ylabel('x residual')
+y1 = fig.add_subplot(2, 2, 3)
+y1.plot(yprojection, '-k')
+y1.plot(dfully * rhovectory, '--r')
+y2 = fig.add_subplot(2, 2, 4)
+y2.plot(yprojection - dfully * rhovectory, '-r')
+y2.set_xlabel('Data point')
+y2.set_ylabel('x residual')
 
 plt.show()
 
+print("TODO: create an actual plot of the calculated phase spaces ")
+
 input()
-
-
-
-
-"""
-for n = 1:nx
-
-    indx   = indxx(n);
-
-    % Set the phase advance
-
-    cosmux = cos(PhaseAdvance_x_y(indx,1));
-    sinmux = sin(PhaseAdvance_x_y(indx,1));
-
-    % Find the indices of the non-zero values of the matrix relating the
-    % projected distribution at YAG02 to the phase space distribution at YAG01
-
-    for xindx1 = 1:psresn
-        for pxindx1 = 1:psresn
-            xindx0  = round(cosmux*(xindx1 - ctroffset) - sinmux*(pxindx1 - ctroffset) + ctroffset);
-            if xindx0>0 && xindx0<=psresn
-                pxindx0 = round(sinmux*(xindx1 - ctroffset) + cosmux*(pxindx1 - ctroffset) + ctroffset);
-                if pxindx0>0 && pxindx0<=psresn
-
-                    dfindxx(:,dfcntrx) = [(n-1)*psresn + xindx1; (xindx0-1)*psresn + pxindx0];
-                    dfcntrx = dfcntrx + 1;
-
-                end
-            end
-        end
-    end
-
-    % Construct the vector of image pixels
-    % why not normalised?
-%     nrm = sum(sum(imagearray[indx]));
-
-    xprojection(((n-1)*psresn+1):n*psresn) = sum(imagearray[indx],2); %/nrm;
-
-end
-"""
-
-
-
-
+input()
+input()
