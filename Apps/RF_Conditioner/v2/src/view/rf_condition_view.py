d811781d3bcc7b7a4550cb96aa088edfc8f3e544
djs56 <duncan.scott@stfc.ac.uk>
2020-07-31 15:38:32 +0100

    Minor Changes...

--- Apps/RF_Conditioner/v2/src/view/rf_condition_view.py
+++ Apps/RF_Conditioner/v2/src/view/rf_condition_view.py
@@ -471,7 +471,8 @@ class rf_condition_view(QMainWindow, Ui_rf_condition_mainWindow):
 
         p_last = self.data.get_kf_running_stat_power_at_set_point(self.values[self.data.last_amp_sp])
         p_current = self.data.get_kf_running_stat_power_at_current_set_point()
-        print('## p_last = {}\np_current = {}'.format(p_last, p_current ))
+        delta_p = self.values[self.data.delta_kfpow]
+        print('## p_last = {}\np_current = {}\ndelta_p = {}'.format(p_last, p_current, delta_p))
 
 
         if p_last:
