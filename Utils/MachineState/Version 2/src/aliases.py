[MachineState] fixed dipole current -> bend angle calculation

Utils/MachineState/Version 2/src/aliases.py
@@ -108,7 +108,11 @@ gun_klystron_fwd_power_vs_momentum = [power, momentum]
 gun_power_to_momentum = scipy.interpolate.interp1d(power,momentum)
 gun_momentum_to_power = scipy.interpolate.interp1d(momentum,power)
 
-# gun momentum measurements taken from \\fed.cclrc.ac.uk\Org\NLab\ASTeC\Projects\VELA\Work\2021\07\28\Linac_power_momentum_scan_cathode22.xls
+# l01 momentum measurements taken from \\fed.cclrc.ac.uk\Org\NLab\ASTeC\Projects\VELA\Work\2021\07\28\Linac_power_momentum_scan_cathode22.xls
+# NOTE: THESE MEASUREMENTS WERE TAKEN USING THE RF METRICS APP (AVG. KLYSTRON POWER BETWEEN THE CURSORS)
+# CLA-GUN-LRF-CTRL-01:ad1:ch1:Power:Wnd:Hi IS THE PEAK IN THE RF TRACE
+# CLA-GUN-LRF-CTRL-01:ad1:ch1:Power:Wnd:Avg IS THE AVERAGE BETWEEN THE CURSOR POSITIONS
+# THE CURSOR POSITIONS WERE 74 AND 117
 power = [7.49*(10**6),8.01*(10**6),8.66*(10**6),9.11*(10**6),9.61*(10**6),1.01*(10**7),1.04*(10**7),1.08*(10**7),1.11*(10**7),1.15*(10**7),1.18*(10**7),1.21*(10**7),1.23*(10**7),1.26*(10**7),1.27*(10**7),1.29*(10**7),1.31*(10**7),1.33*(10**7),1.34*(10**7),1.36*(10**7),1.38*(10**7)]
 mom = [28.73,29.79,30.96,31.70,32.50,33.13,33.56,34.14,34.72,35.19,35.67,36.09,36.35,36.56,36.76,36.93,37.25,37.46,37.51,37.56,37.61]
 momentum = [i - 4.45 for i in mom] #4.45 was gun momentum gain

Utils/MachineState/Version 2/src/unit_conversion.py
@@ -66,7 +66,6 @@ class UnitConversion(object):
                                        field_integral_coefficients[-1])
             self.int_strength = numpy.polyval(self.coeffs, abs(current))
             self.effect = (scipy.constants.speed_of_light / 1e6) * self.int_strength / energy
-            # self.update_widgets_with_values("lattice:" + key + ":k1l", effect / value['magnetic_length'])
             self.k1 = self.effect / (magnetic_length)
             if psu_state == "ON":
                 magdict.update({'k1': float(self.k1)})
@@ -78,8 +77,6 @@ class UnitConversion(object):
             self.coeffs = numpy.append(self.ficmod,
                                        field_integral_coefficients[-1])
             self.int_strength = numpy.polyval(self.coeffs, abs(current))
-            # print(self.int_strength)
-            # print(energy)
             self.field_amplitude = self.int_strength / (10**3 * magnetic_length)
             magdict.update({'field_amplitude': float(int(self.sign) * self.field_amplitude)})
             if psu_state == "ON":
@@ -92,8 +89,6 @@ class UnitConversion(object):
             self.coeffs = numpy.append(self.ficmod,
                                        field_integral_coefficients[-1])
             self.int_strength = numpy.polyval(self.coeffs, abs(current))
-            # print(self.int_strength)
-            # print(energy)
             self.effect = (scipy.constants.speed_of_light / 1e6) * self.int_strength / energy
             magdict.update({'angle': float(self.effect)})
             if psu_state == "ON":
@@ -106,12 +101,10 @@ class UnitConversion(object):
             self.coeffs = numpy.append(self.ficmod,
                                        field_integral_coefficients[-1])
             self.int_strength = numpy.polyval(self.coeffs, abs(current))
-            # print(self.int_strength)
-            # print(energy)
             self.effect = (scipy.constants.speed_of_light / 1e6) * self.int_strength / energy
-            self.angle = numpy.radians(self.effect)
+            self.angle = numpy.degrees(self.effect / 1000)
             if psu_state == "ON":
-                magdict.update({'angle': float(self.effect)})
+                magdict.update({'angle': float(self.angle)})
             else:
                 magdict.update({'angle': 0})
 